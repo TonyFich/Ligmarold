bc20e766643e889d686632df6e79dc6ff9f4d358
Tenegorn <tenegorn@mail.ru>
2015-07-17 00:23:22 +1000

    Клиент
    Добавлено: страница регистрации, логин(изм).

---
+++ public/js/login.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+
+	$('#but_log').click(function() {
+		console.log("message"); 
+            // $.ajax({
+                // url: '/auth',
+                // type: 'POST',
+
+            // });  
+
+    });
+});
