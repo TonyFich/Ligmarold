6bc8e9ea94dd6c2e3202dbd66fafd874c9bab497
Fenixsar <fenixsar@yandex.ru>
2015-07-24 14:15:58 +1000

    Server
    Логин, сессии, страница персонажа.

--- routes/game/index.js
+++ routes/game/index.js
@@ -1,5 +1,13 @@
 module.exports = function (app) {
     app.get('/game', function (req, res) {
         res.render('game/index',{login:req.session.login});
-    });
+    }); 
+
+    app.get('/game/characters', function (req, res) {
+    	if(req.session.login != undefined)
+        	res.render('game/characters',{login:req.session.login});
+        else
+        	res.redirect('/');
+    }); 
+
 };
--- routes/home.js
+++ routes/home.js
@@ -1,21 +1,20 @@
 var db = require("../boot/couchdb").couch;
+var crypto = require('crypto');
+
+var hash = crypto.createHash('sha512');
+hash.update('123124');
+console.log(hash.digest('hex'));
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index');
     });
-    app.get('/reg', function (req, res) {
-        res.render('reg');
-    });
-    //для проверки
-    app.get('/game/index', function (req, res) {
-        res.render('game/index');
-    });
+
     app.post('/auth', function (req, res) {
     	if (!req.body) return res.sendStatus(400);
-    	
 		db.view('users/UsersLogin',{key:req.body.login}, function (err, doc) {
 			if(doc[0]){
+				console.log(crypto.createHash('sha512').update(req.body.password).digest('hex'));
 				if(doc[0].value == req.body.password){
 		    		req.session.login = req.body.login;
 					res.send('yes'); 
@@ -23,6 +22,26 @@ module.exports = function (app) {
 				}
 			}
 			res.send('no');
+		});
+    });
+
+    app.get('/reg', function (req, res) {
+        res.render('reg');
+    });
+    app.post('/reg', function (req, res) {
+    	if (!req.body) return res.sendStatus(400);
+		db.view('users/UsersReg',{key:req.body.login}, function (err, doc) {
+			if(doc[0]){
+				res.send('login');
+			}
+			else{
+				db.view('users/UsersReg',{value:req.body.email}, function (err, doc) {
+					console.log(doc);
+					if(doc[0]){
+						res.send('email');
+					}
+				});
+			}
 		});
     });
 };
