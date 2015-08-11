602f49530e1b8c87dc49ced6da2f929201b6073b
Fenixsar <fenixsar@yandex.ru>
2015-08-11 22:30:35 +1000

    Сервер
    1. Логин теперь это почта.
    2. Изменил структуру пользователя в БД.
    3. Подготовил данные для страницы персонажей.

--- routes/game/index.js
+++ routes/game/index.js
@@ -1,11 +1,20 @@
+var db = require("../../boot/couchdb").couch;
+
 module.exports = function (app) {
     app.get('/game', function (req, res) {
         res.render('game/index',{login:req.session.login});
     }); 
 
     app.get('/game/characters', function (req, res) {
-    	if(req.session.login != undefined)
-        	res.render('game/characters',{login:req.session.login});
+    	if(req.session.id_login != undefined){
+			db.get(req.session.id_login, function (err, doc) {
+				var user = {login:doc.login,
+					gold: doc.gold,
+					wallet: doc.wallet,
+					characters: []}
+	        	res.render('game/characters',{login:user.login});
+			});
+        }
         else
         	res.redirect('/');
     }); 
--- routes/home.js
+++ routes/home.js
@@ -13,7 +13,7 @@ module.exports = function (app) {
 		db.view('users/UsersLogin',{key:req.body.login}, function (err, doc) {
 			if(doc[0]){
 				if(doc[0].value == crypto.createHash('sha512').update(req.body.password).digest('hex')){
-		    		req.session.login = req.body.login;
+		    		req.session.id_login = doc[0].id;
 					res.send('yes'); 
 					return;
 				}
@@ -27,43 +27,34 @@ module.exports = function (app) {
     });
     app.post('/reg', function (req, res) {
     	if (!req.body) return res.sendStatus(400);
-		db.view('users/UsersLogin',{key:req.body.login}, function (err, doc) {
+		db.view('users/UsersLogin',{key:req.body.email}, function (err, doc) {
 			if(doc[0]){
-				res.send('login');
+				res.send('email');
 			}
 			else{
-				db.view('users/UsersReg',{key:req.body.email}, function (err, doc) {
-					if(doc[0]){
-						res.send('email');
+				//Проверка капчи
+				request.post('https://www.google.com/recaptcha/api/siteverify', {form:{secret:config.get("recaptcha"),response:req.body.captcha}}, function optionalCallback(err, httpResponse, body) {
+					if (err) {
+						return;
+					}
+					var answer = JSON.parse(body);
+					if(answer.success == true){
+						req.session.req.session.id_login = req.body.email;
+						//Создание записи в БД
+						db.save({
+							box: 30,
+							gold: 0,
+							login: req.body.email,
+							password: crypto.createHash('sha512').update(req.body.password).digest('hex'),
+							type: "user",
+							wallet: 0
+						}, function (err, res) {
+							// Handle response
+						});
+						res.send('ok');
 					}
 					else{
-						//Проверка капчи
-						request.post('https://www.google.com/recaptcha/api/siteverify', {form:{secret:config.get("recaptcha"),response:req.body.captcha}}, function optionalCallback(err, httpResponse, body) {
-							if (err) {
-								return;
-							}
-							var answer = JSON.parse(body);
-							if(answer.success == true){
-								req.session.login = req.body.login;
-								//Создание записи в БД
-								db.save({
-									box: {"count": 30,"value": []},
-									characters: [],
-									email: req.body.email,
-									gold: 0,
-									login: req.body.login,
-									password: crypto.createHash('sha512').update(req.body.password).digest('hex'),
-									type: "user",
-									wallet: 0
-								}, function (err, res) {
-									// Handle response
-								});
-								res.send('ok');
-							}
-							else{
-								res.send('captcha');
-							}
-						});
+						res.send('captcha');
 					}
 				});
 			}
