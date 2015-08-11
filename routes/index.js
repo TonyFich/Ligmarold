7752146dd16f4895e539e54479ecf4efde484b6a
Fenixsar <fenixsar@yandex.ru>
2015-08-11 23:16:42 +1000

    Сервер
    Создал обработчик для тестовой страницы.

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@
 module.exports = function (app) {
     require("./home")(app);
     require('./game/index')(app);
+
+    app.get('/test', function (req, res) {
+        res.render('index');
+    });
 };
